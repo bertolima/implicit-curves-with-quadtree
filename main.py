9722a8931935d7dc6e7a1341ac379a7e6d1e9bfb
bertolima <bertolilima27@gmail.com>
2022-12-28 14:04:17 -0300

    main update

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ import sys
 
 
 if __name__ == "__main__":
-    #depth controla a pronfudidade maxima inicial da Tree
-    depth = int(sys.argv[1])
-    funcoes = Vetor(12)
-    #esses dois parametros controlam o tamanho das janelas e dos quadrados que representarão as curvas implicitas.
-    #É DE EXTREMA IMPORTÂNCIA QUE ELES SEJAM IGUAIS, CASO CONTRÁRIO A VISUALIZAÇÃO SERÁ PREJUDICADA.
-    #Além disso, se a curva for restrita por valores muito baixos em coordenadas cartesianas, e utilizarmos tamanhos muito grandes de janela, por conta do fato de conversão entre
-    #as coordenadas, as curvas que são limitadas por esse valores baixos serão prejudicadas
+    #width and height must be equal
     width = 512
     height = 512
-
+    #vector that contains the implicit functions
+    funcoes = Vetor(12)
     funcList(funcoes)
+
+    #if didn't got parameter, will initialize with depth 8
+    try:
+        depth = int((sys.argv[1])) #controls max tree depth
+    except:
+        depth = 8
+    
     x = window(depth, funcoes, width, height)
